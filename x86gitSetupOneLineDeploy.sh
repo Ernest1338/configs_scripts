174f06c10c6b58f7bc24170ad8f48451c84b6f00
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2020-09-07 20:31:49 +0200

    Update x86gitSetupOneLineDeploy.sh

--- x86gitSetupOneLineDeploy.sh
+++ x86gitSetupOneLineDeploy.sh
@@ -1,2 +1,3 @@
 # dependencies: wget, docker
+# short one liner: wget -O - https://kutt.it/aMiOg8 | sh
 wget https://raw.githubusercontent.com/Ernest1338/test_repo0002/master/x86gitSetupDockerfile ; docker build -t gitsetup - < x86gitSetupDockerfile ; docker run -d -t -P gitsetup
