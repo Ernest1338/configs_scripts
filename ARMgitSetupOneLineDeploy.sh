6fde48cc090a60b41bdcdfb2c272222f155e69dd
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2020-09-07 21:17:32 +0200

    Update ARMgitSetupOneLineDeploy.sh

--- ARMgitSetupOneLineDeploy.sh
+++ ARMgitSetupOneLineDeploy.sh
@@ -1,3 +1,3 @@
 # dependencies: wget, docker
 # short one liner: wget -O - https://kutt.it/isQ9Ng | sh
-wget https://raw.githubusercontent.com/Ernest1338/test_repo0002/master/ARMgitSetupDockerfile ; docker build -t gitsetup - < ARMgitSetupDockerfile ; docker run -d -t -P gitsetup
+wget https://raw.githubusercontent.com/Ernest1338/test_repo0002/master/ARMgitSetupDockerfile ; sudo docker build -t gitsetup - < ARMgitSetupDockerfile ; sudo docker run -d -t -P gitsetup
