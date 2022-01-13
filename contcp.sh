17d0094b59c6d9ecd11058793f28ede8955a512b
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2022-01-13 23:04:28 +0100

    Update contcp.sh

--- contcp.sh
+++ contcp.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-container=$1
-file=$2
-path=$3
+# Examples:
+# contcp put container_id file
+# contcp put container_id file /path/
+# contcp get container_id /path/
+# contcp get container_id /path/ file
 
-docker start $container 1>/dev/null &&
-if [ -z "$path" ]; then
-	docker cp $file $container:/root/
+getput=$1
+containerid=$2
+arg3=$3
+arg4=$4
+
+docker start $containerid 1>/dev/null &&
+if [ $getput = "get" ]; then
+	if [ -z "$arg4" ]; then
+		docker cp $containerid:$arg3 .
+	else
+		docker cp $containerid:$arg3 $arg4
+	fi
 else
-	docker cp $file $container:$path
+	if [ -z "$arg4" ]; then
+		docker cp $arg3 $containerid:/root/
+	else
+		docker cp $arg3 $containerid:$arg4
+	fi
 fi
-
