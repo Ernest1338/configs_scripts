6dfcee4eea0b2ba5b07f723997bc5bafe934ae4f
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2022-01-22 15:08:44 +0100

    Update emacs-conf1.el

--- emacs-conf1.el
+++ emacs-conf1.el
@@ -1,18 +1,29 @@
 ;; rename this file to init.el to use it
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;; setup evil-mode: cd ~/.emacs.d && git clone --depth 1 https://github.com/emacs-evil/evil.git
+
+;;(require 'package)
+;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;(package-initialize)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-;; M-x package-install RET evil-mode RET
-(evil-mode)
+
+;; deprecated: M-x package-install RET evil-mode RET
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
 (load-theme 'wombat)
+
 (setq initial-buffer-choice 't)
 (setq-default tab-width '4)
 (setq make-backup-files 'nil)
+
 (toggle-frame-maximized)
+
 (load "~/.emacs.d/basic-rust-mode.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
