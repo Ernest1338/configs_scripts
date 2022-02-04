e2d4a46aa45aeca7457a78551c4adf89885c9502
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2022-02-04 01:14:45 +0100

    Update emacs-conf1.el

--- emacs-conf1.el
+++ emacs-conf1.el
@@ -24,16 +24,3 @@
 (toggle-frame-maximized)
 
 (load "~/.emacs.d/basic-rust-mode.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil-visual-mark-mode ##)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
