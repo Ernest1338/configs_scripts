31c8601452812e16e6516334dc4812c8725a0476
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2022-02-15 15:35:40 +0100

    Update lotsoffossv2nojitsi.sh

--- lotsoffossv2nojitsi.sh
+++ lotsoffossv2nojitsi.sh
@@ -2,6 +2,7 @@
 
 cd ~
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y docker.io wget nginx curl nano
 rm /var/www/html/index.nginx-debian.html
 export IP=$(curl ifconfig.me 2>/dev/null)
@@ -20,11 +21,26 @@ docker run -d --name synapse \
     matrixdotorg/synapse:latest
 docker exec synapse bash -c 'echo "" >> /data/homeserver.yaml; echo "enable_registration: true" >> /data/homeserver.yaml'
 docker restart synapse
-wget https://github.com/vector-im/element-web/releases/download/v1.7.34/element-v1.7.34.tar.gz -O ~/element.tar.gz
+# NOTE: Update element-web version number to the latest stable before running
+wget https://github.com/vector-im/element-web/releases/download/v1.10.3/element-v1.10.3.tar.gz -O ~/element.tar.gz
 tar -xzvf element.tar.gz
 mkdir /var/www/html/element/
 mv element*/* /var/www/html/element/
 mv /var/www/html/element/config.sample.json /var/www/html/element/config.json
 echo -e "http://$IP:8008 # Change the base_url to this url and remove this line (you can also change the server_name)\n$(cat /var/www/html/element/config.json)" > /var/www/html/element/config.json
 nano /var/www/html/element/config.json
+echo "Installation Complete"
 cd ~
+
+# TODO:
+# - Privatebin - https://hub.docker.com/r/privatebin/nginx-fpm-alpine
+# - Hedgedoc - https://hub.docker.com/r/linuxserver/hedgedoc
+# - Etherpad - https://hub.docker.com/r/etherpad/etherpad
+# - Ethercalc - https://hub.docker.com/r/audreyt/ethercalc
+# - Cybercheff - https://hub.docker.com/r/mpepping/cyberchef
+# - TT-RSS - https://hub.docker.com/r/lunik1/tt-rss
+# - Invidious - https://hub.docker.com/r/flourgaz/invidious
+# - Nitter - https://hub.docker.com/r/zedeus/nitter
+# - Bibliogram - https://hub.docker.com/r/cloudrac3r/bibliogram
+
+# Note: Nextcloud is a bit heavy
