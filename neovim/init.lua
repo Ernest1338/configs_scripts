aa510ccc98a08f6ffe305ea02b500ebbfd7399e7
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2022-04-26 17:39:33 +0200

    Update init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,4 +1,10 @@
 -- file location: ~/.config/nvim/init.lua
-vim.cmd('set nu')
-vim.cmd('set rnu')
-vim.cmd('syntax on')
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.syntax = "on"
+vim.opt.incsearch = true
+vim.opt.shiftwidth = 4
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
