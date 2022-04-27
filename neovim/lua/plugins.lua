33fb16d7390b5da1b210cd832565716373f68721
Ernest Gupik <ernestgupik@wp.pl>
2022-04-27 20:27:40 +0200

    update nvim plugins

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,3 +1,14 @@
 return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
+        use 'wbthomason/packer.nvim'
+        --[[use {
+          'nvim-telescope/telescope.nvim',
+          requires = { {'nvim-lua/plenary.nvim',
+          'nvim-treesitter/nvim-treesitter'} }
+        }--]]
+        use {
+          'nvim-telescope/telescope.nvim',
+          requires = { {'nvim-lua/plenary.nvim',
+          'nvim-treesitter/nvim-treesitter'} }
+        }
 end)
+
