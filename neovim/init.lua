ed6064d35eb1986169155e03f8fe8367a7c7d70b
Ernest Gupik <ernestgupik@wp.pl>
2022-04-28 21:30:34 +0200

    nvim config update

--- neovim/init.lua
+++ neovim/init.lua
@@ -62,15 +62,15 @@ local cmp = require('cmp')
 cmp.setup{
 	snippet = {
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			require('luasnip').lsp_expand(args.body)
 		end,
 	},
 	sources = {
         {name = 'buffer',option = {keyword_length = 1}}, {name = 'nvim_lsp'}, {name = 'luasnip'}, {name = 'path'},
     },
 	mapping = cmp.mapping.preset.insert({
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-u>'] = cmp.mapping.scroll_docs(4),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
 		['<C-k>'] = cmp.mapping.select_prev_item(),
 		['<C-j>'] = cmp.mapping.select_next_item(),
 		['<C-Space>'] = cmp.mapping.complete(),
