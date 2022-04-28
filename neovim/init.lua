2b8a0cc56065a4d7ae41f3eea0ca248f3d1600a7
Ernest Gupik <ernestgupik@wp.pl>
2022-04-28 20:35:40 +0200

    Update nvim config

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,7 +5,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 -- enable syntax highlighting
-vim.o.syntax = "on"
+vim.o.syntax = 'on'
 
 -- incremental search
 vim.o.incsearch = true
@@ -17,13 +17,19 @@ vim.o.smartindent = true
 vim.o.softtabstop = 4
 vim.o.tabstop = 4
 
+-- hightlight current line
+vim.o.cursorline = true
+
 -- enable mouse support
 vim.o.mouse = 'a'
 
 -- set colorscheme
 vim.o.termguicolors = true
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = 'night'
 vim.cmd('colorscheme tokyonight')
+
+-- set the long line highlight
+vim.o.colorcolumn = '90'
 
 -- remap space as leader key
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -47,4 +53,7 @@ require('nvim-autopairs').setup{}
 
 -- setting up the "magit" plugin
 require('neogit').setup{}
+
+-- setting up the file tree plugin
+require('nvim-tree').setup{}
 
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -20,5 +20,6 @@ return require('packer').startup(function()
 	use 'TimUntersberger/neogit'
     -- auto brackets
     use 'windwp/nvim-autopairs'
+	-- file explorer / file tree
+	use 'kyazdani42/nvim-tree.lua'
 end)
-
