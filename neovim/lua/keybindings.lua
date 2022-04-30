0def904972c2a572acbb43a019f03f28cfbb51db
Ernest Gupik <ernestgupik@wp.pl>
2022-04-30 15:03:08 +0200

    Neovim update keybindings

--- neovim/lua/keybindings.lua
+++ neovim/lua/keybindings.lua
@@ -15,16 +15,40 @@ wk.register({
 			f = { "<cmd>Telescope find_files<cr>", "Find File" },
 			n = { "<cmd>enew<cr>", "New File" },
 			s = { "<cmd>w<cr>", "Save file" },
-			['/'] = { "<cmd>Telescope live_grep<cr>", "Find" },
+			S = { "<cmd>Telescope live_grep<cr>", "Global search" },
+			['/'] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Find" },
 		},
 		q = {
 			name = "Quit",
-			q = { "<cmd>q<cr>", "Are you sure?" },
+			q = { "<cmd>q<cr>", "Quit" },
+			Q = { "<cmd>q!<cr>", "Force quit" },
 		},
 		g = {
 			name = "Git",
 			g = { "<cmd>Neogit<cr>", "Neogit" },
-		}
+		},
+		t = {
+			name = "Telescope",
+			t = { "<cmd>Telescope<cr>", "Telescope" },
+			T = { "<cmd>TodoTelescope<cr>", "Telescope TODO list" },
+			c = { "<cmd>Telescope commands<cr>", "Commands" },
+			s = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+			g = { "<cmd>Telescope git_files<cr>", "Find git files" },
+		},
+		T = { "<cmd>NvimTreeToggle<cr>", "Tree" },
+		s = {
+			name = "Split",
+			v = { "<cmd>vsplit<cr>", "Vertical" },
+			h = { "<cmd>split<cr>", "Horizontal" },
+		},
+		w = {
+			name = "Window",
+			c = { "<cmd>q<cr>", "Close" },
+			h = { "<cmd>wincmd h<cr>", "Left window" },
+			j = { "<cmd>wincmd j<cr>", "Down window" },
+			k = { "<cmd>wincmd k<cr>", "Up window" },
+			l = { "<cmd>wincmd l<cr>", "Right window" },
+		},
 	},
 })
 
