def2c47d990579c23e356b1f930ddacd8bdc4091
Ernest Gupik <ernestgupik@wp.pl>
2022-09-13 16:39:42 +0200

    add wezterm config

---
+++ others/wezterm.lua
@@ -0,0 +1,22 @@
+-- file location: ~/.config/wezterm/wezterm.lua
+return {
+    enable_scroll_bar = true,
+    use_fancy_tab_bar = false,
+    --window_decorations = 'TITLE',
+    window_decorations = 'NONE',
+    hide_tab_bar_if_only_one_tab = true,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
+    default_cursor_style = 'BlinkingBar',
+    color_scheme = 'Gruber (base16)',
+    window_background_opacity = 0.95,
+    tab_bar_at_bottom = true,
+    keys = {
+        { key = 'LeftArrow', mods = 'SHIFT', action = require('wezterm').action.ActivateTabRelative(-1) },
+        { key = 'RightArrow', mods = 'SHIFT', action = require('wezterm').action.ActivateTabRelative(1) },
+    },
+}
