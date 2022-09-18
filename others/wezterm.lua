d7bb4e382ce8837c524f3860c44872fce0b8579c
Ernest Gupik <ernestgupik@wp.pl>
2022-09-19 01:08:59 +0200

    wezterm: change font to 'Hack'

--- others/wezterm.lua
+++ others/wezterm.lua
@@ -1,10 +1,14 @@
 -- file location: ~/.config/wezterm/wezterm.lua
+
+local wt = require('wezterm')
+
 return {
     enable_scroll_bar = true,
     use_fancy_tab_bar = false,
     --window_decorations = 'TITLE',
     window_decorations = 'NONE',
     hide_tab_bar_if_only_one_tab = true,
+    font = wt.font 'Hack',
     window_padding = {
         left = 0,
         right = 0,
@@ -22,10 +26,10 @@ return {
     tab_bar_at_bottom = true,
     scrollback_lines = 2000,
     keys = {
-        { key = 'LeftArrow', mods = 'SHIFT', action = require('wezterm').action.ActivateTabRelative(-1) },
-        { key = 'RightArrow', mods = 'SHIFT', action = require('wezterm').action.ActivateTabRelative(1) },
-        { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = require('wezterm').action.MoveTabRelative(-1) },
-        { key = 'RightArrow', mods = 'SHIFT|CTRL', action = require('wezterm').action.MoveTabRelative(1) },
+        { key = 'LeftArrow', mods = 'SHIFT', action = wt.action.ActivateTabRelative(-1) },
+        { key = 'RightArrow', mods = 'SHIFT', action = wt.action.ActivateTabRelative(1) },
+        { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = wt.action.MoveTabRelative(-1) },
+        { key = 'RightArrow', mods = 'SHIFT|CTRL', action = wt.action.MoveTabRelative(1) },
     },
     -- improves performance?
     cursor_blink_ease_in = "Constant",
