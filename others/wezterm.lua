3743228c07cd2e4337026174046935831f804f2f
Ernest Gupik <ernestgupik@wp.pl>
2022-09-19 19:25:50 +0200

    wezterm: some updates

--- others/wezterm.lua
+++ others/wezterm.lua
@@ -9,6 +9,7 @@ return {
     window_decorations = 'NONE',
     hide_tab_bar_if_only_one_tab = true,
     font = wt.font 'Hack',
+    font_size = 12.5,
     window_padding = {
         left = 0,
         right = 0,
@@ -16,7 +17,10 @@ return {
         bottom = 0,
     },
     default_cursor_style = 'BlinkingBar',
-    color_scheme = 'Gruber (base16)',
+    color_scheme = 'Phrak1 (terminal.sexy)',
+    --color_scheme = 'Gruber (base16)',
+    --color_scheme = 'Pro',
+    --color_scheme = 'PaulMillr',
     --color_scheme = 'Helios (base16)',
     --window_background_image = '/home/dvdmanjaro/Downloads/term_bg.jpg',
     --window_background_image_hsb = {
@@ -24,6 +28,7 @@ return {
     --},
     window_background_opacity = 0.95,
     tab_bar_at_bottom = true,
+    tab_max_width = 50,
     scrollback_lines = 2000,
     keys = {
         { key = 'LeftArrow', mods = 'SHIFT', action = wt.action.ActivateTabRelative(-1) },
@@ -36,4 +41,11 @@ return {
     cursor_blink_ease_out = "Constant",
     animation_fps = 1,
     cursor_blink_rate = 500,
+
+    --unix_domains = {
+        --{
+            --name = 'unix',
+        --},
+    --},
+    --default_gui_startup_args = { 'connect', 'unix' },
 }
