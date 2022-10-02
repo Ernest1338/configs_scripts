0bc305fb54347e80e904cdcf4c9ad712ff3ed4b2
Ernest Gupik <ernestgupik@wp.pl>
2022-10-02 22:40:15 +0200

    nvchad: remove some lua lsp configs

--- neovim/nvchad/custom/plugins/init.lua
+++ neovim/nvchad/custom/plugins/init.lua
@@ -35,14 +35,6 @@ return {
                         diagnostics = {
                             globals = { "vim" },
                         },
-                        workspace = {
-                            library = {
-                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                            },
-                            maxPreload = 100000,
-                            preloadFileSize = 10000,
-                        },
                     },
                 },
             }
