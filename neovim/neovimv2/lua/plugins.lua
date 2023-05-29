4190317948426d9bb616f9ef7add18194cca09cc
Ernest Gupik <ernestgupik@wp.pl>
2023-05-29 15:27:56 +0200

    neovimv2: add configs for zig (zls)

--- neovim/neovimv2/lua/plugins.lua
+++ neovim/neovimv2/lua/plugins.lua
@@ -87,6 +87,10 @@ return {
                     },
                 },
             }
+            nvim_lsp["zls"].setup {
+                on_attach = on_attach,
+                settings = {},
+            }
         end
     },
     {
@@ -242,6 +246,13 @@ return {
             -- require("mini.indentscope").setup {}
             -- require("mini.cursorword").setup { delay = 250 }
             -- require("mini.starter").setup {}
+            -- local hipatterns = require("mini.hipatterns")
+            -- hipatterns.setup {
+            --     highlighters = {
+            --         -- Highlight hex color strings (`#rrggbb`) using that color
+            --         hex_color = hipatterns.gen_highlighter.hex_color(),
+            --     },
+            -- }
         end
     },
     -- { "EtiamNullam/deferred-clipboard.nvim", config = function()
