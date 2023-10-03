7e789d0d10437764870c7315a3b6d5e840dedebb
Ernest Gupik <ernestgupik@wp.pl>
2023-10-03 23:30:07 +0200

    update bashrc + neovimv2 comments

--- neovim/neovimv2/lua/plugins.lua
+++ neovim/neovimv2/lua/plugins.lua
@@ -70,7 +70,6 @@ return {
                         }
                     }
                 }
-
             }
             nvim_lsp["lua_ls"].setup {
                 on_attach = on_attach,
@@ -268,50 +267,51 @@ return {
             --         scroll = 500,
             --     }
             -- }
-            --     local miniclue = require('mini.clue')
-            --     miniclue.setup({
-            --         triggers = {
-            --             -- Leader triggers
-            --             { mode = 'n', keys = '<Leader>' },
-            --             { mode = 'x', keys = '<Leader>' },
+            -- local miniclue = require('mini.clue')
+            -- miniclue.setup({
+            --     triggers = {
+            --         -- Leader triggers
+            --         { mode = 'n', keys = '<Leader>' },
+            --         { mode = 'x', keys = '<Leader>' },
             --
-            --             -- Built-in completion
-            --             { mode = 'i', keys = '<C-x>' },
+            --         -- Built-in completion
+            --         { mode = 'i', keys = '<C-x>' },
             --
-            --             -- `g` key
-            --             { mode = 'n', keys = 'g' },
-            --             { mode = 'x', keys = 'g' },
+            --         -- `g` key
+            --         { mode = 'n', keys = 'g' },
+            --         { mode = 'x', keys = 'g' },
             --
-            --             -- Marks
-            --             { mode = 'n', keys = "'" },
-            --             { mode = 'n', keys = '`' },
-            --             { mode = 'x', keys = "'" },
-            --             { mode = 'x', keys = '`' },
+            --         -- Marks
+            --         { mode = 'n', keys = "'" },
+            --         { mode = 'n', keys = '`' },
+            --         { mode = 'x', keys = "'" },
+            --         { mode = 'x', keys = '`' },
             --
-            --             -- Registers
-            --             { mode = 'n', keys = '"' },
-            --             { mode = 'x', keys = '"' },
-            --             { mode = 'i', keys = '<C-r>' },
-            --             { mode = 'c', keys = '<C-r>' },
+            --         -- Registers
+            --         { mode = 'n', keys = '"' },
+            --         { mode = 'x', keys = '"' },
+            --         { mode = 'i', keys = '<C-r>' },
+            --         { mode = 'c', keys = '<C-r>' },
             --
-            --             -- Window commands
-            --             { mode = 'n', keys = '<C-w>' },
+            --         -- Window commands
+            --         { mode = 'n', keys = '<C-w>' },
             --
-            --             -- `z` key
-            --             { mode = 'n', keys = 'z' },
-            --             { mode = 'x', keys = 'z' },
-            --         },
+            --         -- `z` key
+            --         { mode = 'n', keys = 'z' },
+            --         { mode = 'x', keys = 'z' },
+            --     },
             --
-            --         clues = {
-            --             -- Enhance this by adding descriptions for <Leader> mapping groups
-            --             miniclue.gen_clues.builtin_completion(),
-            --             miniclue.gen_clues.g(),
-            --             miniclue.gen_clues.marks(),
-            --             miniclue.gen_clues.registers(),
-            --             miniclue.gen_clues.windows(),
-            --             miniclue.gen_clues.z(),
-            --         },
-            --     })
+            --     clues = {
+            --         -- Enhance this by adding descriptions for <Leader> mapping groups
+            --         miniclue.gen_clues.builtin_completion(),
+            --         miniclue.gen_clues.g(),
+            --         miniclue.gen_clues.marks(),
+            --         miniclue.gen_clues.registers(),
+            --         miniclue.gen_clues.windows(),
+            --         miniclue.gen_clues.z(),
+            --     },
+            -- })
+            -- NOTE: when mini.places releases, mappings: <l>kc - create, <l>kf - forward, <l>kF - backward
         end
     },
     {
