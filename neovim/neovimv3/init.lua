29df8c5aed54a2fd8406f98d539edede1d3a3227
Ernest Gupik <ernestgupik@wp.pl>
2024-02-13 00:10:04 +0100

    neovimv3: bootstrap mini on first launch

--- neovim/neovimv3/init.lua
+++ neovim/neovimv3/init.lua
@@ -1,12 +1,13 @@
 local path_package = vim.fn.stdpath("data") .. "/site/"
--- NOTE: uncomment on first install
--- local mini_path = path_package .. "pack/deps/start/mini.nvim"
--- if not vim.loop.fs_stat(mini_path) then
---   vim.cmd('echo "Installing `mini.nvim`" | redraw')
---   local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
---   vim.fn.system(clone_cmd)
---   vim.cmd('packadd mini.nvim | helptags ALL')
--- end
+
+-- Bootstrap mini if not already installed
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+end
 
 -- disable some builtin plugins
 local builtin_plugs = {
